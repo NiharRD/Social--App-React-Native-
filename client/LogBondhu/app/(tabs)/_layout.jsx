68241a4620d05ccc78c31014b4ebd433c29dae6e
“NihaR_RD” <niharranjandas008@gmail.com>
2025-10-29 01:33:37 +0530

    layout + icons changed

--- client/LogBondhu/app/(tabs)/_layout.jsx
+++ client/LogBondhu/app/(tabs)/_layout.jsx
@@ -1,13 +1,39 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
+import Feather from "@expo/vector-icons/Feather";
+
 const _layout = () => {
   return (
     <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen name="index" />
-      <Tabs.Screen name="posts" />
-      <Tabs.Screen name="upload" />
-      <Tabs.Screen name="profile" />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="home" color={color} size={size} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="upload"
+        options={{
+          title: "Upload",
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="upload" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="user" color={color} size={size} />
+          ),
+        }}
+      />
     </Tabs>
   );
 };
